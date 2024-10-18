7b206245c092977810f0efac6f1674a501bf5361
Samuelcoder-tester1 <162918679+Samuelcoder-tester1@users.noreply.github.com>
2024-10-18 16:04:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "2348187637779"
+let phoneNumber = "2349065737058"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
